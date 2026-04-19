aedfd6a1ab06741fdbe755ff2efc1f2d406f6957
davidshubitidze01-byte <david.shubitidze01@estudiant.upf.edu>
2026-04-19 17:13:13 +0200

    Did "make f" to fix non-clangformatting

--- src/map_structs.h
+++ src/map_structs.h
@@ -2,20 +2,20 @@
 #define MAP_STRUCTS_H
 
 typedef struct {
-    double lat;
-    double lon;
+  double lat;
+  double lon;
 } Position;
 
 typedef struct House {
-    char street[100];
-    int number;
-    Position pos;
-    struct House *next;
+  char street[100];
+  int number;
+  Position pos;
+  struct House *next;
 } House;
 
 typedef struct Place {
-    char name[100];
-    Position pos;
-    struct Place *next;
+  char name[100];
+  Position pos;
+  struct Place *next;
 } Place;
 #endif
