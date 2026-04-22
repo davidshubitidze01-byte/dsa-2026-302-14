9d5c702e2e4bba99dc902564f39fea03ae42fb13
davidshubitidze01-byte <david.shubitidze01@estudiant.upf.edu>
2026-04-22 18:03:33 +0200

    Added levensthein and being able to choose valid numbers if choosing an invalid one.

--- src/main.c
+++ src/main.c
@@ -6,49 +6,71 @@
 #include <sys/stat.h>
 
 int main() {
-  char map_name[50];
-  printf("Enter map name: ");
-  scanf("%s", map_name);
-  int h_cnt, p_cnt;
-  House *houses = load_houses(map_name, &h_cnt);
-  Place *places = load_places(map_name, &p_cnt);
-  printf("%d houses loades. %d places loaded. \n", h_cnt, p_cnt);
+    char map_name[50];
+    printf("Enter map name: ");
+    scanf("%s", map_name);
+    int h_cnt, p_cnt;
+    // Cargamos los datos de los ficheros a las listas enlazadas
+    House *houses = load_houses(map_name, &h_cnt);
+    Place *places = load_places(map_name, &p_cnt);
+    printf("%d houses loaded. %d places loaded. \n", h_cnt, p_cnt);
+    printf("\n ORIGIN \n Where are you? Address (1), Place (2), Coordinate (3)? ");
+    int opt;
+    scanf("%d", &opt);
+    getchar();
+    if (opt == 1) { //Busqueda por dirección
+        char street[100];
+        int num;
+        printf("Enter street name: ");
+        fgets(street, 100, stdin);
+        street[strcspn(street, "\n")] = 0;
+        printf("Enter street number: ");
+        scanf("%d", &num);
+        House *h = find_house(houses, street, num);
+        if (h) {
+            printf("\nFound at (%f, %f)\n", h->pos.lat, h->pos.lon);
+        } else {
+            //Si no existe la dirección, buscamos si la calle es válida pero el número no
+            int total_nums = 0;
+            int *valid_nums = get_valid_numbers(houses, street, &total_nums);
+            if (total_nums > 0) {
+                printf("Street found, but number %d is invalid. Valid numbers:\n", num);
+                for (int i = 0; i < total_nums; i++) printf("%d ", valid_nums[i]);
+                printf("\nEnter a valid number: ");
+                scanf("%d", &num);
+                h = find_house(houses, street, num);
+                if (h) printf("Found at (%f, %f)\n", h->pos.lat, h->pos.lon);
+            } else {
+                //Si la calle no existe, sugerimos la más parecida usando levenshtein
+                printf("Street not found. Did you mean: ");
+                House *curr = houses;
+                char *best_match = NULL;
+                int min_dist = 999;
+                while (curr) {
+                    int d = levenshtein(street, curr->street);
+                    if (d < min_dist) { min_dist = d; best_match = curr->street; }
+                    curr = curr->next;
+                }
+                if (best_match) printf("%s?\n", best_match);
+            }
+            free(valid_nums);
+        }
+    } else if (opt == 2) { //Búsqueda por lugar
+        char p_name[100];
+        printf("Enter place name: ");
+        fgets(p_name, 100, stdin);
+        p_name[strcspn(p_name, "\n")] = 0;
 
-  printf("\n ORIGIN \n Where are you? Adress (1), Place (2), Coordinate (3)?");
-  int opt;
-  scanf("%d", &opt);
-  getchar();
-  if (opt == 1) {
-    char street[100];
-      int num;
-      printf("Enter street name (e.g. \"Carrer de Roc Boronat\"): ");
-      fgets(street, 100, stdin);
-      street[strcspn(street, "\n")] = 0;
+        Place *p = find_place(places, p_name);
+        if (p) {
+            printf("\nFound at (%f, %f)\n", p->pos.lat, p->pos.lon);
+        } else {
+            printf("Place not found.\n");
+        }
 
-      printf("Enter street number (e.g. \"138\"): ");
-      scanf("%d", &num);
+    } else if (opt == 3) { //Coordenadas
+        printf("Not implemented yet.\n");
+    }
 
-      House *h = find_house(houses, street, num);
-      if (h) {
-          printf("\nFound at (%f, %f)\n", h->pos.lat, h->pos.lon);
-      } else {
-          printf("Address not found.\n");
-      }
-  } else if (opt == 2) {
-    char p_name[100];
-      printf("Enter place name: ");
-      fgets(p_name, 100, stdin);
-      p_name[strcspn(p_name, "\n")] = 0;
-
-      Place *p = find_place(places, p_name);
-      if (p) {
-          printf("\nFound at (%f, %f)\n", p->pos.lat, p->pos.lon);
-      } else {
-          printf("Place not found.\n");
-      }
-  } else if (opt == 3) {
-      printf("Not implemented yet.\n");
-  }
-
-  return 0;
+    return 0;
 }
--- src/map_structs.h
+++ src/map_structs.h
@@ -1,11 +1,12 @@
 #ifndef MAP_STRUCTS_H
 #define MAP_STRUCTS_H
-
+//Estructura para coordenadas
 typedef struct {
   double lat;
   double lon;
 } Position;
 
+//Nodo de lista enlazada para casas
 typedef struct House {
   char street[100];
   int number;
@@ -13,6 +14,7 @@ typedef struct House {
   struct House *next;
 } House;
 
+//nodo de lista enlazada para lugares de interés
 typedef struct Place {
   char name[100];
   Position pos;
