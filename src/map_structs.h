f62e360de8fd961119a24b5265c923d1d234534e
davidshubitidze01-byte <david.shubitidze01@estudiant.upf.edu>
2026-04-28 22:08:07 +0200

    .

--- src/map_structs.h
+++ src/map_structs.h
@@ -1,23 +1,24 @@
 #ifndef MAP_STRUCTS_H
 #define MAP_STRUCTS_H
-// Estructura para coordenadas
+
+//Estructura para las coordenadas
 typedef struct {
   double lat;
   double lon;
 } Position;
 
-// Nodo de lista enlazada para casas
+//Nodo de lista enlazada para las casas
 typedef struct House {
-  char street[100];
-  int number;
-  Position pos;
-  struct House *next;
+  char street[100]; //Nombre de la calle
+  int number; //Numero de la casa
+  Position pos; //Coordenada
+  struct House *next; //Puntero al siguiente nodo
 } House;
 
-// nodo de lista enlazada para lugares de interés
+//nodo de lista enlazada para lugares de interés
 typedef struct Place {
-  char name[100];
-  Position pos;
-  struct Place *next;
+  char name[100]; //nombre
+  Position pos; //Coordenadas
+  struct Place *next; //Puntero al siguiente nodo
 } Place;
 #endif
