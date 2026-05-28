5d4ebc2fcf5d82e249e1d03f7a979a96c46179ab
davidshubitidze01-byte <david.shubitidze01@estudiant.upf.edu>
2026-05-28 11:18:03 +0200

    Changed variable declaration order (due to errors9 and implemented Lab 5 Sequential Search as well as Latency Comparison between Lab4 and Lab5

--- src/main.c
+++ src/main.c
@@ -3,44 +3,68 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <time.h>
 #include "map_utils.h"
 
 int main() {
     char map_name[50];
-    printf("Enter map name: ");
-    scanf("%s", map_name);
     int h_cnt, p_cnt, s_cnt;
-    //Cargamos los datos de los ficheros a las listas enlazadas
-    House *houses = load_houses(map_name, &h_cnt);
-    Place *places = load_places(map_name, &p_cnt);
-    Street *streets = load_streets(map_name, &s_cnt);
-
-    printf("%d houses loaded. %d places loaded. %d streets loaded.\n", h_cnt, p_cnt, s_cnt);
-    printf("\n ORIGIN \n Where are you? Address (1), Place (2), Coordinate (3)? ");
-    int opt;
-    scanf("%d", &opt);
-    getchar();
-
     Position origin_pos;
     int valid_origin = 0;
+    int hash_size;
+    int opt;
+
+    House *houses = NULL;
+    Place *places = NULL;
+    Street *streets = NULL;
+    IntersectionMap *map = NULL;
+    Street *closest = NULL;
+
+    char street[100];
+    int num;
+    int total_nums;
+    int *valid_nums = NULL;
+    House *h = NULL;
+    House *curr = NULL;
+    char *best_match = NULL;
+    int min_dist;
+    char p_name[100];
+    char p_name_guions[120];
+    Place *p = NULL;
+
+    struct timespec start_time, end_time; //Cronometro para el calculo del tiempo de la busqueda secuencial del lab4 y de la busqueda con hasp map del lab5
+    double time_lab4;
+    double time_lab5;
+
+    printf("Enter map name: ");
+    scanf("%s", map_name);
+    houses = load_houses(map_name, &h_cnt);
+    places = load_places(map_name, &p_cnt);
+    streets = load_streets(map_name, &s_cnt);
+    printf("%d houses loaded. %d places loaded. %d streets loaded.\n", h_cnt, p_cnt, s_cnt);
+    
+    hash_size = (int)(s_cnt * 1.5);
+    map = build_intersection_graph(streets, hash_size);
+    
+    printf("\n ORIGIN \n Where are you? Address (1), Place (2), Coordinate (3)? ");
+    scanf("%d", &opt);
+    while (getchar() != '\n');
 
     if (opt == 1) { //Busqueda por dirección
-        char street[100];
-        int num;
         printf("Enter street name: ");
         fgets(street, 100, stdin);
         street[strcspn(street, "\n")] = 0;
         printf("Enter street number: ");
         scanf("%d", &num);
-        House *h = find_house(houses, street, num);
+        h = find_house(houses, street, num);
         if (h) {
             printf("\nFound at (%f, %f)\n", h->pos.lat, h->pos.lon);
             origin_pos = h->pos;
             valid_origin = 1;
         } else {
             //Si no existe la dirección, buscamos si la calle es válida pero el número no
-            int total_nums = 0;
-            int *valid_nums = get_valid_numbers(houses, street, &total_nums);
+            total_nums = 0;
+            valid_nums = get_valid_numbers(houses, street, &total_nums);
             if (total_nums > 0) {
                 printf("Street found, but number %d is invalid. Valid numbers:\n", num);
                 for (int i = 0; i < total_nums; i++) printf("%d ", valid_nums[i]);
@@ -55,9 +79,9 @@ int main() {
             } else {
                 //Si la calle no existe, sugerimos la más parecida usando levenshtein
                 printf("Street not found. Did you mean?: ");
-                House *curr = houses;
-                char *best_match = NULL;
-                int min_dist = 999;
+                curr = houses;
+                best_match = NULL;
+                min_dist = 999;
                 while (curr) {
                     int d = levenshtein(street, curr->street);
                     if (d < min_dist) { min_dist = d; best_match = curr->street; }
@@ -68,11 +92,16 @@ int main() {
             free(valid_nums);
         }
     } else if (opt == 2) { //Búsqueda por lugar
-        char p_name[100];
         printf("Enter place name: ");
         fgets(p_name, 100, stdin);
         p_name[strcspn(p_name, "\n")] = 0;
-        Place *p = find_place(places, p_name);
+        p = find_place(places, p_name);
+        
+        if (!p) {
+            sprintf(p_name_guions, "\"%s\"", p_name);
+            p = find_place(places, p_name_guions);
+        }
+       
         if (p) {
             printf("\nFound at (%f, %f)\n", p->pos.lat, p->pos.lon);
             origin_pos = p->pos;
@@ -87,10 +116,28 @@ int main() {
         valid_origin = 1;
     }
     if (valid_origin && streets) {
-        Street *closest = find_closest_street(streets, origin_pos);
-        if (closest) {
+        closest = find_closest_street(streets, origin_pos);
+        if (closest) {//Comenzamos la medicion de tiempo de la busqueda en el Lab4 y en el Lab5
+            printf("Busqueda Secuencial del Lab 4 \n");
+            clock_gettime(CLOCK_MONOTONIC, &start_time);
             print_connected_streets(streets, closest);
+            clock_gettime(CLOCK_MONOTONIC, &end_time);
+            time_lab4 = (end_time.tv_sec - start_time.tv_sec) * 1000000 + (end_time.tv_nsec - start_time.tv_nsec) / 1000;
+            
+            printf("Busqueda Secuencial del Lab 5 \n");
+            clock_gettime(CLOCK_MONOTONIC, &start_time);
+            print_connected_streets_hash(map, closest);
+            clock_gettime(CLOCK_MONOTONIC, &end_time);
+            time_lab5 = (end_time.tv_sec - start_time.tv_sec) * 1000000 + (end_time.tv_nsec - start_time.tv_nsec) / 1000;
+
+            printf("Map size (Streets):  %d\n", s_cnt);
+            printf("Latencia Lab 4: %.2f microsegons\n", time_lab4);
+            printf("Latencia Lab 5:   %.2f microsegons\n", time_lab5);
         }
+    
+    }
+    if (map) {
+        free_hash_map(map);
     }
     return 0;
 }
